a1fec45811bf4a025ac12bfb46eb2f411dfd6f6e
grvrjt <gaurav1993rajput@gmail.com>
2021-01-14 10:59:49 +0530

    FEAT:delete and search a hero

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -9,7 +9,7 @@ import { Hero } from '../hero'
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  @Input() hero!: Hero;
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -27,6 +27,11 @@ export class HeroDetailComponent implements OnInit {
   }
   goBack():void{
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 
 }
